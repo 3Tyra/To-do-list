ea5c31555e89e80685ad9469daa257d043c816b2
Tyra <tyra.mwai@student.moringaschool.com>
2025-04-25 10:54:43 +0300

    Finalized my project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, text: "Pay Internet Bill" },
+    { id: 2, text: "Settle Electricity Bill" },
+    { id: 3, text: "Cancel Netflix" },
+    { id: 4, text: "Team Lunch" },
+    { id: 5, text: "Book Uber" }
+  ]);
+
+  const [taskToEdit, setTaskToEdit] = useState(null);
+
+  const addTask = (task) => {
+    setTasks([...tasks, { id: Date.now(), text: task }]);
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id));
+  };
+
+  const editTask = (id, updatedText) => {
+    setTasks(tasks.map(task =>
+      task.id === id ? { ...task, text: updatedText } : task
+    ));
+    setTaskToEdit(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrapper">
+      <h1 className="app-heading">To-do-List</h1>
+      <TaskForm addTask={addTask} taskToEdit={taskToEdit} editTask={editTask} />
+      <TaskList tasks={tasks} onDelete={deleteTask} onEdit={setTaskToEdit} />
     </div>
   );
 }
